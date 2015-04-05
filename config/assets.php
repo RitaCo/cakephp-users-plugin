5cd89c0aadbc56cedff44543b1eb5a12cecfacf0
zoghal <saleh.souzanchi@gmail.com>
2015-04-05 13:21:25 +0430

    rename settings.php to assets.php

---
+++ config/assets.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'css' => [
+        'base' => [
+        ],
+        'front' => [
+            'Rita/Core.debug.css',
+            'Rita/Users.users-front.css'
+        ],
+        'client' => [
+            'Rita/Users.users-front.css'
+        ],
+        'admin' => [
+            'Rita/Users.users-admin.css'',
+        ],
+    ],
+    'js' => [
+        'base' => [
+        ],    
+        'front' => [
+        ],
+        'client' => [
+        ],
+        'admin' => [
+        ],
+    ]
+    
+ ];
